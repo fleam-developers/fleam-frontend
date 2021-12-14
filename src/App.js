098c96f6376551bb2a43edb4c3658d02d3840a19
Mert Can Demir <validatedev@gmail.com>
2021-12-14 19:35:41 +0300

    Add initial theming

--- src/App.js
+++ src/App.js
@@ -1,11 +1,56 @@
 import './App.css';
 import MainRouter from "./router/MainRouter";
+import React from 'react';
+import {createTheme, ThemeProvider} from '@material-ui/core/styles';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import useMediaQuery from "@material-ui/core/useMediaQuery";
+import { ThemeOptions } from '@material-ui/core/styles/createTheme';
+
+export const darkTheme: ThemeOptions = {
+  palette: {
+    type: 'dark',
+    primary: {
+      main: '#202020',
+    },
+    secondary: {
+      main: '#ff0000',
+    },
+  },
+};
+
+export const lightTheme: ThemeOptions = {
+  palette: {
+    type: 'light',
+    primary: {
+      main: '#202020',
+    },
+    secondary: {
+      main: '#ff0000',
+    },
+  },
+};
+
+function Theme() {
+  const prefersDarkMode = useMediaQuery('(prefers-color-scheme: dark)');
+  const theme = React.useMemo(
+    () =>
+      createTheme(
+        prefersDarkMode ? darkTheme : lightTheme,
+      ),
+    [prefersDarkMode],
+  );
+  return theme;
+
+}
 
 function App() {
   return (
+    <ThemeProvider theme={Theme()}>
     <div className="App">
+      <CssBaseline/>
       <MainRouter />
     </div>
+    </ThemeProvider>
   );
 }
 
